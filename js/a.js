7e2a98eba7ca862416f5807b466dc1adacec7816
Michael Cahyadi <36390867+maikxchd@users.noreply.github.com>
2022-03-01 23:07:51 +0700

    added js

---
+++ js/a.js
@@ -0,0 +1,6 @@
+var AdobeTracking = new Object();
+AdobeTracking.contentType = 'Home';
+AdobeTracking.pageName = 'Hioctane : Home';
+AdobeTracking.showSite = 'of3tg4rxpe';
+AdobeTracking.showSiteFeatureII = 'Hioctane';
+_satellite.pageBottom();
